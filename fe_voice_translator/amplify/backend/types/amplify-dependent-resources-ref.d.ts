0e0140c86028048aab0962a19cd817e9fb640e80
miketran <miketran@amazon.com>
2023-05-04 09:20:02 -0700

    [feat] - handover

--- fe_voice_translator/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ fe_voice_translator/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "voicetranslatoreast34e3ceef": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
